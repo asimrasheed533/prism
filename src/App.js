7a3ef80519f4dedec43c409d4b7416548c33e1da
Asim Rasheed <asimrasheed442@gmail.com>
2023-06-08 23:56:55 +0500

    onboarding1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Onboarding1 from "./screens/Onboarding1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/onboarding1" element={<Onboarding1 />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
---
+++ src/screens/Onboarding1.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import prism from "../assets/prism.svg";
+export default class Onboarding1 extends Component {
+  render() {
+    return (
+      <div className="main__body">
+        <div className="header__conatiner">
+          <img src={prism} alt="logo" />
+        </div>
+        <div className="text__container">
+          <div className="text__container__heading">
+            Connection your world of work
+          </div>
+          <div className="text__container__sub__heading">
+            Get connected with the best startrups. Build your profile and
+            reputation. Get hired by the best companies.
+          </div>
+        </div>
+        <div className="main__btn">
+          <button className="btn__primary">Get Started</button>
+        </div>
+        <div className="key__points">
+          <div className="key__points__container">
+            <div className="key__points__svg">
+              <svg
+                width="53"
+                height="53"
+                viewBox="0 0 53 53"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.1128 53C15.6083 53.0009 14.1655 52.1725 13.1026 50.6976L0.978426 33.9087C-0.326142 32.101 -0.326142 29.1706 0.978426 27.3629C2.28341 25.5557 4.39879 25.5557 5.70378 27.3629L17.1128 43.1673L47.2962 1.35538C48.6012 -0.451792 50.7166 -0.451792 52.0216 1.35538C53.3261 3.16312 53.3261 6.09348 52.0216 7.90123L21.123 50.6976C20.0601 52.1725 18.6172 53.0009 17.1128 53Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+            <div className="key__points__text">Key benefit and feature</div>
+          </div>
+          <div className="key__points__container">
+            <div className="key__points__svg">
+              {" "}
+              <svg
+                width="53"
+                height="53"
+                viewBox="0 0 53 53"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.1128 53C15.6083 53.0009 14.1655 52.1725 13.1026 50.6976L0.978426 33.9087C-0.326142 32.101 -0.326142 29.1706 0.978426 27.3629C2.28341 25.5557 4.39879 25.5557 5.70378 27.3629L17.1128 43.1673L47.2962 1.35538C48.6012 -0.451792 50.7166 -0.451792 52.0216 1.35538C53.3261 3.16312 53.3261 6.09348 52.0216 7.90123L21.123 50.6976C20.0601 52.1725 18.6172 53.0009 17.1128 53Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+            <div className="key__points__text">Key benefit and feature</div>
+          </div>
+          <div className="key__points__container">
+            <div className="key__points__svg">
+              {" "}
+              <svg
+                width="53"
+                height="53"
+                viewBox="0 0 53 53"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.1128 53C15.6083 53.0009 14.1655 52.1725 13.1026 50.6976L0.978426 33.9087C-0.326142 32.101 -0.326142 29.1706 0.978426 27.3629C2.28341 25.5557 4.39879 25.5557 5.70378 27.3629L17.1128 43.1673L47.2962 1.35538C48.6012 -0.451792 50.7166 -0.451792 52.0216 1.35538C53.3261 3.16312 53.3261 6.09348 52.0216 7.90123L21.123 50.6976C20.0601 52.1725 18.6172 53.0009 17.1128 53Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+            <div className="key__points__text">Key benefit and feature</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
