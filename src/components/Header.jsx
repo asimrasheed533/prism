e1a194530596fca4c1e134bddde9aef1d85a47c7
Asim Rasheed <asimrasheed442@gmail.com>
2023-06-12 16:41:59 +0500

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,89 +19,92 @@ export default class Header extends Component {
 
   render() {
     return (
-      <div className="header__conatiner__onboarding">
-        <Link to="/home" className="header__img">
-          <img src={prism} alt="logo" />
-        </Link>
-        <div className="menu__dp">
-          <button
-            title="Menu"
-            onClick={() => {
-              this.setState({ showMenu: !this.state.showMenu });
-            }}
-          >
-            {this.state.showMenu ? (
-              <svg
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13 1L1 13"
-                  stroke="black"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M1 1L13 13"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            ) : (
-              <svg
-                width="20"
-                height="14"
-                viewBox="0 0 20 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 7H19"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M1 1H19"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M1 13H19"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            )}
-          </button>
-          <div className="option__profile">
-            <img src={dp} alt="avatar" />
-          </div>
-        </div>
-        {this.state.showMenu && (
-          <div className="option__profle__menu">
-            <div className="option__menu">
-              <div className="option__menu__entry">Wallet</div>
-              <div className="option__menu__entry">Profile</div>
-              <div className="option__menu__entry">jobs</div>
-              <div className="option__menu__entry">Community</div>
-              <div className="option__menu__entry">Contact</div>
-              <div className="option__menu__entry">Logout</div>
+      <>
+        <div className="header__conatiner__onboarding">
+          <Link to="/home" className="header__img">
+            <img src={prism} alt="logo" />
+          </Link>
+          <div className="menu__dp">
+            <button
+              title="Menu"
+              onClick={() => {
+                this.setState({ showMenu: !this.state.showMenu });
+              }}
+            >
+              {this.state.showMenu ? (
+                <svg
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13 1L1 13"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M1 1L13 13"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  width="20"
+                  height="14"
+                  viewBox="0 0 20 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 7H19"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M1 1H19"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M1 13H19"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              )}
+            </button>
+            <div className="option__profile">
+              <img src={dp} alt="avatar" />
             </div>
           </div>
-        )}
-      </div>
+          {this.state.showMenu && (
+            <div className="option__profle__menu">
+              <div className="option__menu">
+                <div className="option__menu__entry">Wallet</div>
+                <div className="option__menu__entry">Profile</div>
+                <div className="option__menu__entry">jobs</div>
+                <div className="option__menu__entry">Community</div>
+                <div className="option__menu__entry">Contact</div>
+                <div className="option__menu__entry">Logout</div>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="border__header"></div>
+      </>
     );
   }
 }
